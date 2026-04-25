5373c81e774caf44b02d380a9ecd1c70079d20ef
Flexksx <cristian.cretu2808@gmail.com>
2026-04-25 11:38:58 +0300

    add scaffolding for the project

---
+++ backend/platform-api/platform-api-client/src/main/java/org/flexksx/platformapiclient/PlatformApiClientApplication.java
@@ -0,0 +1,13 @@
+package org.flexksx.platformapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformApiClientApplication.class, args);
+	}
+
+}
---
+++ backend/platform-api/platform-api-service/src/test/java/org/flexksx/platformapiservice/PlatformApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flexksx.platformapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
