cbbea1791a3c0932d022dff53220066fb1c3b4e6
Flexksx <cristian.cretu2808@gmail.com>
2026-04-25 11:47:15 +0300

    update

--- backend/platform-api/platform-api-client/src/main/java/org/flexksx/platformapiclient/PlatformApiClientApplication.java
+++ backend/platform-api/platform-api-client/src/main/java/org/flexksx/platformapiclient/PlatformApiClientApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlatformApiClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlatformApiClientApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(PlatformApiClientApplication.class, args);
+  }
 }
--- backend/platform-api/platform-api-service/src/main/java/org/flexksx/platformapiservice/PlatformApiServiceApplication.java
+++ backend/platform-api/platform-api-service/src/main/java/org/flexksx/platformapiservice/PlatformApiServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlatformApiServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlatformApiServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(PlatformApiServiceApplication.class, args);
+  }
 }
